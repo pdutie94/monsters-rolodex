a39584ace07a296d7b7a7250ce177a460fab15d3
pdutie94 <ptiencntt94@gmail.com>
2021-02-25 15:54:47 +0700

    First commit

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,11 @@
+import react from 'react'
+
+import './search-box.style.css'
+
+export const SearchBox = ({placeholder, handleChange}) => (
+    <input
+          type="search"
+          placeholder={placeholder}
+          onChange={handleChange}
+        />
+)
